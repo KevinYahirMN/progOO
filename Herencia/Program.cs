7325becf6e279cf6e2dea88ef34428961d69859e
KevinYahirMN <47574080+KevinYahirMN@users.noreply.github.com>
2019-02-26 10:31:15 -0800

    herencia

---
+++ Herencia/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Herencia
+{
+
+class Musico
+{
+    public string nombre;
+    public Musico(string n){
+
+        nombre = n;
+    }
+
+    public string saludo()
+    {
+     return string.Format("Hola, soy {0}", nombre) ;
+    }
+}
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Musico m = new Musico("Lady gaga");
+
+        Console.WriteLine(m.saludo());
+        }
+    }
+}
