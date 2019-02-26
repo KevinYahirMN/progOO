e6a2cffc1fd0736c53aa0ca0a8f59176128140d2
KevinYahirMN <47574080+KevinYahirMN@users.noreply.github.com>
2019-02-26 10:51:12 -0800

    Update Program.cs

--- Herencia/Program.cs
+++ Herencia/Program.cs
@@ -11,18 +11,48 @@ class Musico
         nombre = n;
     }
 
-    public string saludo()
+    public virtual string saludo()
     {
      return string.Format("Hola, soy {0}", nombre) ;
     }
-}
+    }
+
+    class Bajista : Musico
+    {
+        public string bajo;
+          public Bajista (string nombre, string bajo):base(nombre){
+          this.bajo = bajo;
+     }
+     public override string saludo()
+     {
+        return base.saludo() + "y soy bajista";
+     }
+    }
+
+    class Pianista : Musico
+    {
+        public string piano;
+        public Pianista (string nombre, string piano):base(nombre){
+            this.piano = piano;
+        }
+        public override string saludo()
+        {
+            return base.saludo() + "y soy pianista";
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
         Musico m = new Musico("Lady gaga");
+        Bajista b = new Bajista("el rapsodia boemia ", "cereza");
+        Pianista p = new Pianista("Lucas muecas ","perfect");
+
 
         Console.WriteLine(m.saludo());
+        Console.WriteLine(b.saludo());
+        Console.WriteLine(p.saludo());
         }
     }
 }
