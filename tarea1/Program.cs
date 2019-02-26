369b96088ff8ec6f6634c4f978a35af84c2268cb
KevinYahirMN <47574080+KevinYahirMN@users.noreply.github.com>
2019-02-26 10:08:10 -0800

    Update Program.cs

--- tarea1/Program.cs
+++ tarea1/Program.cs
@@ -3,41 +3,30 @@ using System.Collections.Generic;
 
 namespace tarea1
 {
-   class Persona
-    {
-        //Atributos
-        protected string nombre;
-        protected int edad;
-        //Se hacen los datos propiedad
-        public string Nombre
-        {
-            get {return nombre;}
-            set {nombre=value;}
-        }
-        //La clase también tiene métodos
-        //El primer método será un constructor, la mayoría de los métodos son públicos
-        public Persona (string nombre, int e)
-        {
-            //Se usa el this. para diferenciar entre las variables locales y las que se encuentran en la clase, si no hubiera ambiguedad, no se necesita el this
-            this.nombre=nombre;
-            this.edad=e;
-        }
+   class Persona{
+protected string nombre;
+protected int edad;
 
-        //Método para imprimir, void porque no retorna valor
-        public virtual void Imprime()
-        {
-            Console.WriteLine(nombre);
+public string Nombre {
+get { return nombre;}
+set { nombre = value; }
+}
 
-        }
+public Persona(string nombre, int e )
+{
+this.nombre = nombre;
+edad = e;
+}
+public virtual void Imprime()
+{
+Console.WriteLine(nombre);
+}
 
-        //Sobrecarga de imprime, se llama igual, pero tiene argumentos o tipos de datos distintos
-        public void Imprime(int veces)
-        {
-            for (int i=0;i<veces;i++)
-            {
-                Imprime();
-            }
-        }
+public void Imprime(int veces){
+for (int i=0; i<veces; i++ )
+Imprime();
+}
+
         public static Persona operator +(Persona a, Persona b)
         {
             return new Persona(a.nombre+b.nombre, a.edad+b.edad);
@@ -58,39 +47,38 @@ namespace tarea1
     }
 
     class Alumno:Persona
-    {
-    private string num_control;
-    
-    public Alumno(string nombre, int edad,
-    string n):base(nombre,edad)
-    {
-       num_control = n;
-       
-    }
-    public override void Imprime()
-    {
-        Console.WriteLine(num_control);
-    }
-    }
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            //Se crea una lista para los objetos tipo persona, la lista se llama personas
-            List<Persona>personas=new List<Persona>();
-           
-            //Aquí se crean los objetos, esto se hace después de hacer el constructor en la clase Persona
-            Persona p=new Persona("Ana",46);
+{
+private string num_control;
 
-            //Se agrega Ana a la lista
-            personas.Add(new Alumno("kevas",19,"18212211"));
-            //Igualmente, se pueden instanciar nuevos objetos dentro de una lista
-            personas.Add(new Persona("ki",12));
-            p.Imprime(3);
-            personas[1].Imprime();
+public Alumno(string nombre, int edad
+, string n ):base(nombre,edad)
+{
+// this.nombre = nombre;
+// this.edad = edad;
+this.num_control = n; }
+public override void Imprime()
+{
+Console.WriteLine(num_control);
+}
 
-            Alumno a = new Alumno("Lola", 20, "123423311");
-            a.Imprime();
-        }
-    }
+}
+
+
+class program
+{
+static void Main(string[] args)
+{
+Persona p = new Persona("Ana",46);
+List<Persona> personas = new List<Persona>();
+personas.Add(p);
+//personas.Add("Jim");
+personas.Add( new Alumno("Jim",18,"18210782"));
+personas.Add( new Persona("Ki",12));
+p.Imprime(3);
+personas[1].Imprime();
+Alumno a = new Alumno("Zoe",19,"ZZ");
+a.Imprime();
+// Console.WriteLine("Helo Wsd!");
+}
+}
 }
