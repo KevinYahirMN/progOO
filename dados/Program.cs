529eba84e9b73ec14d702b7a675cb548278a3b06
KevinYahirMN <47574080+KevinYahirMN@users.noreply.github.com>
2019-02-27 23:43:19 -0800

    tarea

---
+++ dados/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace dados
+{
+    class Dado 
+    {
+        public int valor;
+        public string color;
+
+public Dado (int valor, string color)
+{
+    this.valor = valor;
+    this.color = color;
+}
+         public void Imprime()
+        {
+            Console.WriteLine(valor.ToString());
+            Console.WriteLine(color);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dado dado1 = new Dado(5,"verde");
+            Dado dado2 = new Dado(3,"azul");
+            Dado dado3 = new Dado(1,"rojo");
+            dado1.Imprime();
+        }
+    }
+}
