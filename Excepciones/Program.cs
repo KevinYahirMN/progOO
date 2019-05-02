41a4e8f5969a5c9e9a00efd150b9c049f06d7f60
KevinYahirMN <47574080+KevinYahirMN@users.noreply.github.com>
2019-05-02 10:07:00 -0700

    todo lo que no eh guardado en un mes :V

---
+++ Excepciones/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Excepciones
+{
+    class EdadIncorrectaException : Exception
+    {
+        public EdadIncorrectaException(): base(error){}
+
+        public EdadIncorrectaException(): base("Edad muy alta"){}
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Captura un numero");
+
+            int num = 0;
+            string captura;
+
+            try 
+            {
+                captura = Console.ReadLine();
+                num = Int32.Parse(captura);
+            
+                Console.WriteLine("Se capturo el numero {0}",captura);
+            }
+
+            catch(EdadIncorrectaException)
+            {
+                
+            }
+            catch(Exception x)
+            {
+                Console.Write(x.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Captura un numero we");
+                
+            }
+            
+        }
+    }
+}
